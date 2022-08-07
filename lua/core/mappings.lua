0786b2a24e49fe9f3645827c1a34d9c959fa6c54
yonchicy <1079279184@qq.com>
2022-08-07 09:22:39 +0800

    添加一些常用插件

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -150,10 +150,7 @@ M.comment = {
 
   v = {
     ["<C-_>"] = {
-      function()
-        require("Comment.api").toggle_current_linewise()
-      end,
-
+      "<ESC><cmd>lua require('Comment.api').toggle_linewise_op(vim.fn.visualmode())<CR>",
       "蘒  toggle comment",
     },
     ["<leader>/"] = {
@@ -230,12 +227,12 @@ M.lspconfig = {
       "lsp code_action",
     },
 
-    ["gr"] = {
-      function()
-        vim.lsp.buf.references()
-      end,
-      "   lsp references",
-    },
+    -- ["gr"] = {
+    --   function()
+    --     vim.lsp.buf.references()
+    --   end,
+    --   "   lsp references",
+    -- },
 
     ["ge"] = {
       function()
